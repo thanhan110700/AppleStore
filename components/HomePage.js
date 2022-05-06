c77e86e02c74ce8c6778236ce19b57ba55a88a20
Thanh An <lvuthanhan1107@gmail.com>
2022-05-06 11:26:24 +0700

    upload

---
+++ components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    Image,
+    Text,
+    View
+} from 'react-native'
+function HomePage(){
+    return <View>
+        <Text>
+            Homepage
+        </Text>
+    </View>
+}
+export default HomePage
